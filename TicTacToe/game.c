75735786ab37c594a0b40436ff0336e8bf73b82d
mon1tor <westxlebyshek@gmail.com>
2023-09-17 19:05:24 +0200

    Basic interaction

--- TicTacToe/game.c
+++ TicTacToe/game.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #include "game.h"
 
+#define PLAYER_SIGN Cross
+#define COMPUTER_SIGN Zero
+
 CellState matrix[FIELD_SIZE][FIELD_SIZE];
+bool isGameOver = false;
+
+void PerformComputerMove(AIType);
+void CheckGameOver();
 
 void InitializeGame()
 {
 	srand(time());
 	for (int i = 0; i < FIELD_SIZE; ++i)
 		for (int j = 0; j < FIELD_SIZE; ++j)
-			matrix[i][j] = rand() > RAND_MAX / 2.0f ? Cross : Zero;
+			matrix[i][j] = None;
 }
 
 void SetCellState(int xCell, int yCell, CellState state)
@@ -19,8 +26,18 @@ void SetCellState(int xCell, int yCell, CellState state)
 	matrix[xCell][yCell] = state;
 }
 
+CellState GetCellState(int xCell, int yCell)
+{
+	if (xCell < 0 || xCell >= FIELD_SIZE || yCell < 0 || yCell >= FIELD_SIZE)
+		return None;
+	return matrix[xCell][yCell];
+}
+
 void HandleMouseClick(int mouseX, int mouseY, int viewport[4])
 {
+	if (isGameOver)
+		return;
+
 	float x = (mouseX - viewport[0]) / (float)viewport[2];
 	float y = 1.0f - (mouseY - viewport[1]) / (float)viewport[3];
 
@@ -29,5 +46,57 @@ void HandleMouseClick(int mouseX, int mouseY, int viewport[4])
 
 	int xCell = (int)(FIELD_SIZE * x);
 	int yCell = (int)(FIELD_SIZE * y);
-	SetCellState(xCell, yCell, None);
+
+	if (GetCellState(xCell, yCell))
+		return;
+
+	SetCellState(xCell, yCell, PLAYER_SIGN);
+
+	CheckGameOver();
+
+	PerformComputerMove(Easy);
+}
+
+void PerformComputerMove(AIType type)
+{
+	if (isGameOver)
+		return;
+
+	if (type == Easy)
+	{
+		int emptyCells[FIELD_SIZE * FIELD_SIZE] = { 0 };
+		int emptyCellsCount = 0;
+
+		for (int i = 0; i < FIELD_SIZE; ++i)
+			for (int j = 0; j < FIELD_SIZE; ++j)
+				if (!matrix[i][j])
+					emptyCells[emptyCellsCount++] = (i << 16) | j;
+
+		int cell = emptyCells[rand() % emptyCellsCount];
+
+		int i = (cell >> 16) & 0xFFFF;
+		int j = cell & 0xFFFF;
+
+		SetCellState(i, j, COMPUTER_SIGN);
+	}
+
+	CheckGameOver();
+}
+
+void CheckGameOver()
+{
+	bool gameOver = true;
+
+	for (int i = 0; gameOver && i < FIELD_SIZE; ++i)
+		for (int j = 0; j < FIELD_SIZE; ++j)
+		{
+			if (!matrix[i][j])
+			{
+				gameOver = false;
+				break;
+			}
+		}
+
+	if (gameOver)
+		isGameOver = true;
 }
--- TicTacToe/game.h
+++ TicTacToe/game.h
@@ -3,9 +3,11 @@
 #define FIELD_SIZE 3
 
 typedef enum { None = 0, Cross = 1, Zero = 2 } CellState;
+typedef enum { Easy = 0, Medium = 1, Hard = 2 } AIType;
 
 extern CellState matrix[FIELD_SIZE][FIELD_SIZE];
 
 void InitializeGame();
 void SetCellState(int, int, CellState);
+CellState GetCellState(int, int);
 void HandleMouseClick(int, int, int[4]);
